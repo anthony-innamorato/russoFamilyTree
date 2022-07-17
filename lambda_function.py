4dd58205be67dee327c5e3e9887720ea1c2fe7b9
Anthony Innamorato <ainnamor@ainnamor-mn1.linkedin.biz>
2022-07-17 01:52:47 -0400

    poc done

--- lambda_function.py
+++ lambda_function.py
@@ -38,7 +38,7 @@ def put(email, body):
   return response
   
 def getDefaultTree():
-  return {'Item': {'email': {'S': 'ai974@nyu.edu'}, 'tree': {'S': '      {"class": "go.TreeModel", "nodeDataArray": [{"key":1, "name":"Stella Payne Diaz", "title":"CEO"}]}'}}, 'ResponseMetadata': {'RequestId': 'T8SSG3LCNS2KGAVFV1GNNEK9QRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Sun, 17 Jul 2022 03:17:15 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '528', 'connection': 'keep-alive', 'x-amzn-requestid': 'T8SSG3LCNS2KGAVFV1GNNEK9QRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '1270102284'}, 'RetryAttempts': 0}}
+  return {'Item': {'email': {'S': 'ai974@nyu.edu'}, 'tree': {'S': '{ "class": "",  "nodeDataArray": [{"key":1,"name":"Stella Payne Diaz","title":"CEO"},{"name":"(new person)","title":"","comments":"","parent":1,"key":2},{"name":"(new person)","title":"","comments":"","parent":2,"key":3},{"name":"(new person)","title":"","comments":"","parent":2,"key":4}],  "linkDataArray": [{"from":1,"to":2},{"from":1,"to":3},{"from":2,"to":4}]}'}}, 'ResponseMetadata': {'RequestId': 'T8SSG3LCNS2KGAVFV1GNNEK9QRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'HTTPStatusCode': 200, 'HTTPHeaders': {'server': 'Server', 'date': 'Sun, 17 Jul 2022 03:17:15 GMT', 'content-type': 'application/x-amz-json-1.0', 'content-length': '528', 'connection': 'keep-alive', 'x-amzn-requestid': 'T8SSG3LCNS2KGAVFV1GNNEK9QRVV4KQNSO5AEMVJF66Q9ASUAAJG', 'x-amz-crc32': '1270102284'}, 'RetryAttempts': 0}}
   
 def get(email):
   data = client.get_item(
